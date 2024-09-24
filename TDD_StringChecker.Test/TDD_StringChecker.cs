bd7b79faa131c2aa93763526f26db075dd759c38
Eduard_wer <eduard.unanov66@mail.ru>
2024-09-24 13:00:38 +0300

    sdfhsdkg

---
+++ TDD_StringChecker.Test/TDD_StringChecker.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using NUnit.Framework;
+using TDD_StringCheck;
+
+namespace TDD_StringChecker.Test
+{
+    [TestFixture]
+    public class TDD_StringChecker
+    {
+        [Test]
+        public void shortword_hello_word_wordexepted()
+        {
+
+            string str = "hello word";
+
+            TDD_StringCheckers stringChecker = new TDD_StringCheckers();
+
+            StringAssert.IsMatch(stringChecker.shortwords(str), "word");
+        }
+
+
+        [Test]
+        public void wordLeight_hello_word_2exepted() // колво слов в строке 
+        {
+            string str = "hello word";
+
+            TDD_StringCheckers stringChecker = new TDD_StringCheckers();
+
+            Assert.AreEqual(stringChecker.wordLeight(str), 2);
+        }
+
+        [Test]
+        public void wordCount_hello_word_10exepted()
+        {
+            string str = "hello word";
+
+            TDD_StringCheckers stringChecker = new TDD_StringCheckers();
+
+            Assert.AreEqual(stringChecker.wordCount(str), 10);
+        }
+
+
+        [Test]
+        public void wordNight_hello_word_rexepted()
+        {
+            string str = "hello word";
+
+            TDD_StringCheckers stringChecker = new TDD_StringCheckers();
+
+            Assert.AreEqual(stringChecker.wordNight(str), 'r');
+        }
+
+
+        [Test]
+        public void ints_hjdfkgh12345hddfgk_5exepted()
+        {
+            string str = "hjdfkgh12345hddfgk";
+
+            TDD_StringCheckers stringChecker = new TDD_StringCheckers();
+
+            Assert.AreEqual(stringChecker.ints(str), 5);
+        }
+
+
+
+        [Test]
+        public void ints_podrad_edsd123sdfsf_3exepted()
+        {
+            string str = "edsd123sdfsf";
+
+
+            TDD_StringCheckers stringChecker = new TDD_StringCheckers();
+
+            Assert.AreEqual(stringChecker.ints_count(str), 3);
+        }
+
+
+        [Test]
+        public void search_am_hfdkjghkamdfam_2exepted()
+        {
+            string str = "hfdkjghkamdfam";
+            string substring = "am";
+            TDD_StringCheckers stringChecker = new TDD_StringCheckers();
+
+            Assert.AreEqual(stringChecker.search_am(str,substring), 2);
+        }
+    }
+
+
+}
---
+++ TDD_StringCheckу/TDD_StringCheckers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace TDD_StringCheck
+{
+    public class TDD_StringCheckers
+    {
+        public string shortwords (string str_input)
+        {
+            var words = str_input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length == 0)
+            {
+                return null;
+            }
+            return words.OrderBy(w => w.Length).First();
+        }
+
+
+        public int wordLeight(string str_input)
+        {
+            return str_input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public int wordCount(string str_input)
+        {
+            return str_input.Length;
+        }
+
+        public char? wordNight(string str_input)
+        {
+            if (str_input.Length < 9)
+            {
+                return null;
+            }
+
+            return str_input[8];
+        }
+
+        public int ints(string str_input)
+        {
+            int count = 0;
+            foreach (char c in str_input)
+            {
+                if (char.IsDigit(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+
+        public int ints_count(string str_input)
+        {
+            int maxCount = 0;
+            int currentCount = 0;
+
+            foreach (char c in str_input)
+            {
+                if (char.IsDigit(c))
+                {
+                    currentCount++;
+                    if (currentCount > maxCount)
+                    {
+                        maxCount = currentCount;
+                    }
+                }
+                else
+                {
+                    currentCount = 0;
+                }
+            }
+
+            return maxCount;
+        }
+
+        public int search_am(string str_input, string substr)
+        {
+            int count = 0;
+            int index = 0;
+
+            while ((index = str_input.IndexOf(substr, index)) != -1)
+            {
+                count++;
+                index += substr.Length;
+            }
+
+            return count;
+        }
+        
+        
+    }
+}
